36bd47cad461c88d51253469f114658617fd93d7
Rodrigo Lecinana <devrodrigolec@gmail.com>
2023-12-05 17:40:20 +0100

    buscador y boton agregados

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,17 @@
-import './App.css'
+import "./App.css";
 
 function App() {
-  
-
   return (
     <>
-    
+      <header>
+        <h1>WikiPelis</h1>
+        <form action="" className="form">
+          <input className="buscador" type="text" placeholder="Batman, Spiderman, Shrek..." />
+          <button type="submit">Buscar</button>
+        </form>
+      </header>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
