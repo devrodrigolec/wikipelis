ee858bab460ff478bc735eb307d1edc29bf96fe1
Rodrigo Lecinana <devrodrigolec@gmail.com>
2023-12-05 18:18:51 +0100

    mostrar peliculas, titulo y poster realizado

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,8 @@
 import "./App.css";
+import responseAPI from './mocks/with-results.json'
 
 function App() {
+  const movies = responseAPI.Search;
   return (
     <>
       <header>
@@ -10,6 +12,23 @@ function App() {
           <button type="submit">Buscar</button>
         </form>
       </header>
+      <main>
+        <ul className="ul-grid">
+        {
+          movies.map((movie) => {
+            return(
+              <li className="grid-item" key={movie.imdbID}>
+                <h3>{movie.Title}</h3>
+                <p>{movie.Year}</p>
+                <img src={movie.Poster} alt="" />
+              </li>
+             
+            )
+          })
+        }
+
+        </ul>
+      </main>
     </>
   );
 }
